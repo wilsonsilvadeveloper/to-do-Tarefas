733875eacfc2ad52147018667ca06fdb99114e9a
Wilson <wilsonoficial.com@gmail.com>
2023-02-08 13:30:58 -0300

    novo arquivo js (verifica tamanho da janela)

---
+++ js/tamanho-da-janela.js
@@ -0,0 +1,33 @@
+function verificaTamanhoJanela() {
+    var tamanho = document.documentElement.clientWidth;
+    mudaLegenda(tamanho)
+}
+
+function mudaLegenda(tamanhoDaJanela) {
+    var LegendaNome = document.querySelector('#legenda-nome');
+    var LegendaDeletar = document.querySelector('#legenda-deletar');
+    var LegendaData = document.querySelector('#legenda-data');
+    var LegendaConcluido = document.querySelector('#legenda-concluido');
+
+    if(tamanhoDaJanela < 900) {
+        LegendaNome.textContent = 'Nome';
+        LegendaDeletar.textContent = 'Excluir';
+        LegendaData.textContent = 'Conclusão';
+        
+
+        if(tamanhoDaJanela < 700) {
+            LegendaConcluido.textContent = '';
+        } else {
+            LegendaConcluido.textContent = 'Concluido';
+        }
+    } else {
+        LegendaNome.textContent = 'Nome da Tarefa';
+        LegendaDeletar.textContent = 'Deletar Tarefa';
+        LegendaData.textContent = 'Data de Conclusão';
+        LegendaConcluido.textContent = 'Concluido';
+    }
+
+
+}
+
+setInterval(verificaTamanhoJanela, 500);
